91dd4eaa41276ca828f241fa487783ca35f8e731
surajsharma <surajsharma@pm.me>
2023-02-25 18:57:36 +0530

    css cleanup

--- defaultSettings.js
+++ defaultSettings.js
@@ -1,3 +1,6 @@
+/*
+TODO: css cleanup
+*/
 var defaultSettings = {
   currentPage: "Home",
   options: {
@@ -5,98 +8,98 @@ var defaultSettings = {
       preview: {
         label: "Preview on hover",
         classes: [
-          "div.style-scope.ytd-video-preview",
+          ".ytd-video-preview",
           "#mouseover-overlay",
-          "span.style-scope.ytd-thumbnail-overlay-loading-preview-renderer",
+          ".ytd-thumbnail-overlay-loading-preview-renderer"
         ],
-        show: true,
+        show: true
       },
       communityPosts: {
         label: "Latest posts",
         classes: ["ytd-rich-shelf-renderer"],
         id: "communityPosts",
-        show: true,
+        show: true
       },
       adThumbs: {
         label: "Ad Thumbnails",
         classes: [".ytd-display-ad-renderer", ".ytd-ad-slot-renderer"],
-        show: true,
+        show: true
       },
       chipBar: {
         label: "Feed Filter Chip Bar",
         classes: [".ytd-feed-filter-chip-bar-renderer"],
-        show: true,
+        show: true
       },
       title: {
         label: "Video title",
         classes: ["yt-formatted-string.style-scope.ytd-rich-grid-media"],
-        show: true,
-      },
+        show: true
+      }
     },
     Video: {
       sidebar: {
         label: "Video Sidebar",
         classes: [".ytd-watch-next-secondary-results-renderer"],
-        show: true,
+        show: true
       },
       nextvideos: {
         label: "End Recs (Default)",
         classes: [".ytp-ce-video .ytp-ce-channel .ytp-ce-covering-overlay"],
-        show: true,
+        show: true
       },
       endvideos: {
         label: "End Recs (Channel)",
         classes: [".ytp-endscreen-content"],
-        show: true,
+        show: true
       },
       chat: {
         label: "Chat",
         classes: ["#chat"],
-        show: true,
+        show: true
       },
       likes: {
         label: "Likes",
         classes: [
-          "ytd-menu-renderer.style-scope.ytd-watch-metadata .yt-core-attributed-string",
+          "ytd-menu-renderer.style-scope.ytd-watch-metadata .yt-core-attributed-string"
         ],
-        show: true,
+        show: true
       },
       comments: {
         label: "Comments",
         classes: [".ytd-comments"],
-        show: true,
+        show: true
       },
       playlist: {
         label: "Playlist",
         classes: ["div.style-scope.ytd-playlist-panel-renderer"],
-        show: true,
+        show: true
       },
       chapters: {
         label: "Chapters",
         classes: [
-          "ytd-engagement-panel-section-list-renderer.style-scope.ytd-watch-flexy",
+          "ytd-engagement-panel-section-list-renderer.style-scope.ytd-watch-flexy"
         ],
-        show: true,
+        show: true
       },
       subscribe: {
         label: "Subscribe Button",
         classes: ["yt-button-shape.style-scope.ytd-subscribe-button-renderer"],
-        show: true,
+        show: true
       },
       title: {
         label: "Video Title",
         classes: ["yt-formatted-string.style-scope.ytd-watch-metadata"],
-        show: true,
+        show: true
       },
       sub_count: {
         label: "Subscriber count",
         classes: ["yt-formatted-string.style-scope.ytd-video-owner-renderer"],
-        show: true,
+        show: true
       },
       description: {
         label: "Description Box",
         classes: ["ytd-text-inline-expander.style-scope.ytd-watch-metadata"],
-        show: true,
+        show: true
       },
       merch: {
         label: "Merchandise Box",
@@ -113,43 +116,47 @@ var defaultSettings = {
           "yt-icon.style-scope.ytd-badge-supported-renderer",
           "div.badge.badge-style-type-live-now-alternate.style-scope.ytd-badge-supported-renderer",
           "#metadata-line",
-          "#byline-container",
+          "#byline-container"
         ],
-        show: true,
+        show: true
       },
       duration: {
         label: "Video Duration",
         classes: [
-          "span.style-scope.ytd-thumbnail-overlay-time-status-renderer",
+          "span.style-scope.ytd-thumbnail-overlay-time-status-renderer"
         ],
-        show: true,
+        show: true
       },
       thumbnails: {
         label: "Video Thumbnails",
-        classes: [".ytd-macro-markers-list-item-renderer>img",".thumbnail-container.style-scope.ytd-notification-renderer",".yt-core-image--loaded"],
-        show: true,
+        classes: [
+          ".ytd-macro-markers-list-item-renderer>img",
+          ".thumbnail-container.style-scope.ytd-notification-renderer",
+          ".yt-core-image--loaded"
+        ],
+        show: true
       },
       resume: {
         label: "Resume bar",
         classes: [
-          "div.style-scope.ytd-thumbnail-overlay-resume-playback-renderer",
+          "div.style-scope.ytd-thumbnail-overlay-resume-playback-renderer"
         ],
-        show: true,
+        show: true
       },
       logo: {
         label: "YouTube Logo",
         classes: ["#logo .ytd-topbar-logo-renderer"],
-        show: true,
+        show: true
       },
       channelThumb: {
         label: "Channel Avatar",
         classes: [
           "#avatar",
           "#channel-thumbnail",
-          "tp-yt-paper-item.style-scope.ytd-guide-entry-renderer > yt-img-shadow",
+          "tp-yt-paper-item.style-scope.ytd-guide-entry-renderer > yt-img-shadow"
         ],
-        show: true,
-      },
-    },
-  },
+        show: true
+      }
+    }
+  }
 };
