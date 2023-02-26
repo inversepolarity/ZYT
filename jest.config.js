573168a45d04d1302cab654c3c0efaf5738d8424
surajsharma <surajsharma@pm.me>
2023-02-26 18:38:15 +0530

    testing scaffold

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  verbose: true,
+  rootDir: "tests",
+  testTimeout: 50000
+};
---
+++ tests/bootstrap.js
@@ -0,0 +1,37 @@
+const puppeteer = require("puppeteer");
+
+async function bootstrap(options = {}) {
+  const { devtools = false, slowMo = false, appUrl } = options;
+  const browser = await puppeteer.launch({
+    headless: false,
+    devtools,
+    args: [
+      "--disable-extensions-except=./ZenTube",
+      "--load-extension=./ZenTube"
+    ],
+    ...(slowMo && { slowMo })
+  });
+
+  const appPage = await browser.newPage();
+  await appPage.goto(appUrl, { waitUntil: "load" });
+
+  const targets = await browser.targets();
+  const extensionTarget = targets.find(
+    (target) => target.type() === "service_worker"
+  );
+  const partialExtensionUrl = extensionTarget.url() || "";
+  const [, , extensionId] = partialExtensionUrl.split("/");
+
+  const extPage = await browser.newPage();
+  const extensionUrl = `chrome-extension://${extensionId}/options/options.html`;
+  await extPage.goto(extensionUrl, { waitUntil: "load" });
+
+  return {
+    appPage,
+    browser,
+    extensionUrl,
+    extPage
+  };
+}
+
+module.exports = { bootstrap };
