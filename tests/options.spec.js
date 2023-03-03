6e04b5754b4f3e611dc24fe41a45d1f319fc4e60
surajsharma <surajsharma@pm.me>
2023-03-03 12:58:32 +0530

    test:settings stored

--- tests/options.spec.js
+++ tests/options.spec.js
@@ -15,10 +15,17 @@ describe("test suite for options popup", () => {
     appPage = context.appPage;
     browser = context.browser;
     chrome.runtime.getManifest.mockImplementation(() => manifest);
+    global.chrome = {
+      storage: {
+        local: {
+          set: jest.fn(() => defaultSettings),
+          get: jest.fn(() => defaultSettings)
+        }
+      }
+    };
   });
 
-  it("check manifest version", async () => {
-    await extPage.bringToFront();
+  it("manifest version", async () => {
     const ver = await extPage.$("#version");
     const verText = await ver.evaluate((e) => e.innerText);
 
@@ -26,22 +33,26 @@ describe("test suite for options popup", () => {
     expect(chrome.runtime.getManifest).toBeCalled();
   });
 
-  it("check ip link event listener", async () => {
+  it("ip link event listener", async () => {
     await extPage.click("#icon");
     const donationPage = await getNewBrowserTab(browser);
-    await donationPage.bringToFront();
     expect(donationPage.url()).toBe("https://ko-fi.com/evenzero");
     donationPage.close();
   });
 
-  it("check if transitions css injected", async () => {
+  it("transitions css injected", async () => {
     const transitions = await appPage.$("#zentubeTransitions");
     expect(transitions).toBeTruthy();
   });
 
-  it("check if toggle css injected", async () => {
+  it("toggle css injected", async () => {
     const zentube = await appPage.$("#zentube");
     expect(zentube).toBeTruthy();
+  });
+
+  it("settings stored", async () => {
+    const gettingStoredSettings = global.chrome.storage.local.get();
+    expect(gettingStoredSettings).toBeTruthy();
   });
 
   afterAll(async () => {
